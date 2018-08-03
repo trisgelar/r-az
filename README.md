def14bb1097a940bbce5c2ee2d11f34f0880efd2
Trisna Gelar <trisgelar@gmail.com>
2018-08-03 15:12:44 +0700

    section 4 - matrices part 2

---
+++ README.md
@@ -0,0 +1,3 @@
+# README
+
+Kode-kode latihan: kursus online R Programming di Udemy
--- matrix.R
+++ matrix.R
@@ -90,3 +90,26 @@ matplot(t(Data), type="b", pch=15:18, col=c(1:4,6))
 legend("bottomleft", inset=0.01, legend=Players[1], col=c(1:4,6), pch=15:18, horiz = F)
 
 
+myplot <- function(data, rows=1:10){
+    Data <- data[rows,,drop=F]
+    matplot(t(Data), type="b", pch=15:18, col=c(1:4,6))
+    legend("bottomleft", inset=0.01, legend=Players[rows], col=c(1:4,6), pch=15:18, horiz = F)
+}
+
+myplot(MinutesPlayed/Games, 3)
+myplot(Salary)
+
+#Salary
+myplot(Salary)
+myplot(Salary / Games)
+myplot(Salary / FieldGoals)
+
+#In-Game Metrics
+myplot(MinutesPlayed)
+myplot(Points)
+
+#In-Game Metrics Normalized
+myplot(FieldGoals/Games)
+myplot(FieldGoals/FieldGoalAttempts)
+myplot(FieldGoalAttempts/Games)
+myplot(Points/Games)
